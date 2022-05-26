f804a79469d29991fd58df13020a2eeeb7e621fd
itaji-create <itaji.1999@gmail.com>
2022-05-26 11:50:41 -0300

    cria classe abstrata Archetype

---
+++ src/Archetypes/Archetype.ts
@@ -0,0 +1,20 @@
+import { EnergyType } from '../Energy';
+
+abstract class Archetype {
+  private _name: string;
+  private _special: number;
+  private _cost: number;
+  constructor(readonly name: string, readonly special: number = 0, readonly cost: number = 0) {
+    this._name = name;
+    this._cost = cost;
+    this._special = special;
+  }
+
+  static createdArchetypeInstances() {
+    throw new Error('Not implemented');
+  }
+
+  abstract get energyType(): EnergyType;
+}
+
+export default Archetype;
