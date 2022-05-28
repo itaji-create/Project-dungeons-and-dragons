65060b83700a20d39621acc0bde6052a43d24a3d
itaji-create <itaji.1999@gmail.com>
2022-05-27 22:59:26 -0300

    cria classe Dragons

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,10 @@
+// import Character from '../Character';
+// import Battle from './Battle';
+
+// class PVP extends Battle {
+//   constructor(char1: Character, char2: Character) {
+//     super(char1);
+//   }
+// }
+
+// export default PVP;
